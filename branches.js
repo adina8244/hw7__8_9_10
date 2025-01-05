20975ce2a17598cedc075a3e9809775830453b20
sara8192-git <38328241377@mby.co.il>
2025-01-05 14:20:56 +0200

    delete&add func

--- branches.js
+++ branches.js
@@ -8,18 +8,13 @@ const a = () => {
 const b = () => {
     return 10
 }
-
-const cc = () => {
-    return 10
-}
-const ppgit = () => {
+const aaaaaaaaaaa = () => {
     return 10
 }
 
-const qqqqq = () => {
+const bbbbbbbbb = () => {
     return 10
 }
-const wwww = () => {
-    return 10
-}
+
+
 <>"add conflict"</>
