5f2ac74c92da8ccf488295948fd2f5e90bd343b2
שרה הלוי <328241377@mbydomain>
2025-01-05 13:46:14 +0200

    some changes

--- branches.js
+++ branches.js
@@ -1,11 +1,15 @@
 <h>this is from hw 7</h>;
 <h2>"this is from branch2" </h2>
 
-const a=()=>{
-    return 10}
-    <>"add conflict"</>
+const a = () => {
+    return 10
+}
 
-const aa=()=>{
-        return 10}
-        <>"add conflict"</>
-    
+const b = () => {
+    return 10
+}
+
+const cc = () => {
+    return 10
+}
+<>"add conflict"</>
